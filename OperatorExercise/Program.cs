912726af811a1843a84042d2bb9bcc77f4400407
Myles Hill <mylehill99@gmail.com>
2023-09-07 21:03:09 -0400

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,16 @@
 ﻿namespace OperatorExercise
 {
+    "17/4 is 4 remainder 1
+    'What is 'radius' of your circle?
+20
+'The area of a circle with radius of 20 is 1256.6370614359173
+press any key to Continue...'
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is the radius of your cice?");
+            var radius = double.Parse(Console.ReadLine());
         }
     }
 }
