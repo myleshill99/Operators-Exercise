77607ed5f7d921670aeb95af2a7a51c6d071d004
Myles Hill <mylehill99@gmail.com>
2023-10-03 12:25:01 -0400

    finished project

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,16 +1,33 @@
 ﻿namespace OperatorExercise
 {
-    "17/4 is 4 remainder 1
-    'What is 'radius' of your circle?
-20
-'The area of a circle with radius of 20 is 1256.6370614359173
-press any key to Continue...'
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is the radius of your cice?");
+            // Exercise 1 
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            int addition = a + b;
+            int subtraction = a - b;
+            int multipication = a * b;
+
+            Console.WriteLine($"Addition is {addition}");
+            Console.WriteLine($"Subtraction is {subtraction}");
+            Console.WriteLine($"Multipication is {multipication}");
+            Console.WriteLine($"Division is {quotient}");
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+
+            // Exercise 2 
+
+            Console.WriteLine("What is the radius of your circle?");
             var radius = double.Parse(Console.ReadLine());
+            var answer = (Math.PI * Math.Pow(radius, 2));
+            Console.WriteLine($"The area of a circle with radius of {radius} is {answer}");
         }
     }
 }
